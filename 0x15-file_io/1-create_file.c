c3954329fb9761bf5cd4fa4314b2a71c8c6cc38b
djphemi <djphemi@gmail.com>
2022-05-02 20:41:27 -0700

    v

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,42 @@
 #include "main.h"
 
 /**
- * create_file - a function that creates a file
- *
- * @filename: name of file to create
- * @text_content: string to write to file
- *
- * Return: 1 on success OR -1 on faliure
-*/
+  * create_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, write_status, words = 0;
+	int fd;
 
-	if (filename == NULL) /*check if filename is present*/
+	if (!filename)
 		return (-1);
 
-	/*open file by creating it and if it exists write but truncate to 0*/
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file == -1) /*check if file creation was a success*/
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
-	if (text_content) /*write content to file if its not NULL*/
-	{
-		while (text_content[words] != '\0') /*find number of words*/
-			words++;
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 
-		/*write to file*/
-		write_status = write(file, text_content, words);
-		if (write_status == -1) /*check if write was a success*/
-			return (-1);
-	}
-
-	close(file); /*close file*/
+	close(fd);
 	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
